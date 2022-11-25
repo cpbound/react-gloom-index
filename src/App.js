5cec1370bd45d6b53e90997bffcefc7b854d5272
Christian Bound <automaticbound@gmail.com>
2022-11-25 16:48:51 +0900

    buttons added and styled. json is readable. Playlist picker outputs range

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import './App.css';
-import GloomInput from './compnents/GloomInput';
+import gloomIndex from "./gloomIndex"
+
+import { ButtonGroup } from '@mui/material';
+import { Button } from '@mui/material';
+
+console.log(gloomIndex)
+
+const PlaylistPicker = () => {
+  console.log("Hello world")
+  const range = Array.from({length: 10}, (_, i) => i + 1)
+  console.log(range)
+}
+
+PlaylistPicker()
 
 function App() {
   return (
     <div className="App">
-      Hello World
-      <GloomInput input={"After years of waiting..."} />
+      <h1>Hello World</h1>
+      <div>
+        <ButtonGroup fullWidth={true} variant="contained" aria-label="outlined primary button group">
+          <Button>One</Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+          <Button>Four</Button>
+          <Button>Five</Button>
+        </ButtonGroup>
+      </div>
+      <div>
+        <ButtonGroup fullWidth={true} variant="contained" aria-label="outlined primary button group">
+          <Button>Six</Button>
+          <Button>Seven</Button>
+          <Button>Eight</Button>
+          <Button>Nine</Button>
+          <Button>Ten</Button>
+        </ButtonGroup>
+      </div>
+
     </div>
   );
 }
