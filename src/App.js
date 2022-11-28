5dd26bb7d260f8db9db7e35b7cf1b5a055bcc022
Christian Bound <automaticbound@gmail.com>
2022-11-28 17:58:02 +0900

    playlist picker working in console

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
 import './App.css';
+import { useState } from 'react';
 import gloomIndex from "./gloomIndex"
 
 import { ButtonGroup } from '@mui/material';
 import { Button } from '@mui/material';
 
-console.log(gloomIndex)
-
-const PlaylistPicker = () => {
-  console.log("Hello world")
-  const range = Array.from({length: 10}, (_, i) => i + 1)
-  console.log(range)
-}
-
-PlaylistPicker()
+const radiohead = gloomIndex.radiohead
+const range = (start, stop, step) =>
+  Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step)
 
 function App() {
+
+  const [userInput, setUserInput] = useState(0);
+
+  const PlaylistPicker = () => {
+    const userChoice = range(userInput - 10, userInput + 10, 1)
+    const res = radiohead.filter(song => userChoice.includes(Math.round(song.gloom_index)))
+    console.log(res.map(song => song.track_name))
+  }
+
+  PlaylistPicker()
+
   return (
     <div className="App">
       <h1>Hello World</h1>
       <div>
         <ButtonGroup fullWidth={true} variant="contained" aria-label="outlined primary button group">
-          <Button>One</Button>
-          <Button>Two</Button>
-          <Button>Three</Button>
-          <Button>Four</Button>
-          <Button>Five</Button>
+          <Button onClick={() => setUserInput(10)}>One</Button>
+          <Button onClick={() => setUserInput(20)}>Two</Button>
+          <Button onClick={() => setUserInput(30)}>Three</Button>
+          <Button onClick={() => setUserInput(40)}>Four</Button>
+          <Button onClick={() => setUserInput(50)}>Five</Button>
         </ButtonGroup>
       </div>
       <div>
         <ButtonGroup fullWidth={true} variant="contained" aria-label="outlined primary button group">
-          <Button>Six</Button>
-          <Button>Seven</Button>
-          <Button>Eight</Button>
-          <Button>Nine</Button>
-          <Button>Ten</Button>
+          <Button onClick={() => setUserInput(60)}>Six</Button>
+          <Button onClick={() => setUserInput(70)}>Seven</Button>
+          <Button onClick={() => setUserInput(80)}>Eight</Button>
+          <Button onClick={() => setUserInput(90)}>Nine</Button>
+          <Button onClick={() => setUserInput(100)}>Ten</Button>
         </ButtonGroup>
       </div>
 
