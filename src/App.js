8f29e553623398f7899536031f5228ea41f96f39
Christian Bound <automaticbound@gmail.com>
2022-12-26 13:03:45 +0900

    bottomnav added w/ margin. styled a little. deploy now and tinker with it later possibly.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import gloomIndex from "./gloomIndex";
 import PlaylistPicker from "./components/PlaylistPicker";
 import ButtonPicker from "./components/ButtonPicker";
 import * as React from "react";
-
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import CottageIcon from '@mui/icons-material/Cottage';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import AttributionIcon from '@mui/icons-material/Attribution';
 
 const radiohead = gloomIndex.radiohead;
 const range = (start, stop, step) =>
@@ -19,6 +23,7 @@ function App() {
     const newPlaylist = radiohead.filter((song) =>
       userChoice.includes(Math.round(song.gloom_index))
     );
+    newPlaylist.sort();
     setCurrentPlaylist(newPlaylist);
   };
 
@@ -30,12 +35,22 @@ function App() {
     <div className="App">
       <h1>Radiohead Gloom Index</h1>
       <h2>On a scale of 😫0 - 10😆, how are you feeling today?</h2>
+      <h3>Created using data from <a href="https://www.rcharlie.com/images/blog/fitter-happier/album_chart.html">here.</a> Read more about it <a href="https://www.rcharlie.com/blog/fitter-happier/">here.</a></h3>
       <ButtonPicker
         userInput={setUserInput}
         array={currentPlaylist}
         playlist={playlist}
       />
       <PlaylistPicker array={currentPlaylist} />
+      <BottomNavigation
+        sx={{backgroundColor: "whitesmoke", position: "fixed", bottom: 0, left: 0, right: 0 }}
+        elevation={1}
+        showLabels
+      >
+        <BottomNavigationAction label="Project Github" icon={<GitHubIcon />} href="https://github.com/cpbound/react-gloom-index" />
+        <BottomNavigationAction label="My Homepage" icon={<CottageIcon />} href="https://crsbnd.netlify.app/" />
+        <BottomNavigationAction label="2022 Chris Bound" icon={<AttributionIcon />} href="https://github.com/cpbound" />
+      </BottomNavigation>
     </div>
   );
 }
--- src/components/PlaylistPicker.js
+++ src/components/PlaylistPicker.js
@@ -1,3 +1,4 @@
+import "../scss/playlistPicker.scss";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -7,34 +8,39 @@ import { Grid } from "@mui/material";
 
 const PlaylistPicker = (props) => {
   return (
-    <Grid container spacing={1}>
-      {props.array.map((song) => (
-        <Grid item xs={2} key={song.track_name}>
-          <Grow
-            in={true}
-            style={{ transformOrigin: "0 0 0" }}
-            {...(props.array ? { timeout: 1000 } : {})}
-          >
-            <Card>
-              <CardMedia
-                component="img"
-                maxwidth="100px"
-                image={song.album_img}
-                alt="album image"
-              />
-              <CardContent>
-                <Typography variant="body2" component="div">
-                  {song.track_name}
-                </Typography>
-                <Typography variant="caption" color="text.secondary">
-                  {song.album_name}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grow>
-        </Grid>
-      ))}
-    </Grid>
+    <div className="playarray">
+      <Grid container spacing={1}>
+        {props.array.map((song) => (
+          <Grid item xs={2} key={song.track_name}>
+            <Grow
+              in={true}
+              style={{ transformOrigin: "0 0 0" }}
+              {...(props.array ? { timeout: 1000 } : {})}
+            >
+              <Card>
+                <CardMedia
+                  component="img"
+                  maxwidth="100px"
+                  image={song.album_img}
+                  alt="album image"
+                />
+                <CardContent>
+                  <Typography variant="body2" component="div">
+                    {song.track_name}
+                  </Typography>
+                  <Typography variant="caption" color="text.secondary">
+                    {song.album_name} - {song.album_release_year}
+                  </Typography>
+                  <Typography variant="caption" component="div">
+                    {song.gloom_index}% happy
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grow>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
   );
 };
 
